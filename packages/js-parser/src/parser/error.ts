027802752af906688bd72525e61637562c799446
Solo-steven <ssteven19@kimo.com>
2023-05-31 13:38:29 +0800

    chore: add forgot stuff in last commit

--- packages/js-parser/src/parser/error.ts
+++ packages/js-parser/src/parser/error.ts
@@ -4,6 +4,7 @@ export const ErrorMessageMap = {
     getter_can_not_be_async_or_generator: "getter can not be async or generator",
     setter_should_has_at_last_one_params: "setter must at last has one param",
     setter_can_not_be_async_or_generator: "setter can not be async or generator",
+    constructor_can_not_be_async_or_generator: "constructor in class can not be a async or generator",
     tag_template_expression_can_not_use_option_chain: "Tag template expression should not using optional chain",
     private_field_can_not_use_in_object: "Private field '#test' must be declared in an enclosing class",
     rest_element_should_be_last_property: "RestElement should be the lastest property",
