e3e1190e8d25e23ed0062f518d8557e29d7d8b69
Solo-steven <ssteven19@kimo.com>
2023-06-09 13:13:54 +0800

    feat: finish basic playground error

--- packages/web-frontend/src/components/Playground/Playground.tsx
+++ packages/web-frontend/src/components/Playground/Playground.tsx
@@ -1,9 +1,10 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, Fragment } from "react";
 import CodeMirror from "@uiw/react-codemirror";
+import { Dialog, Transition } from "@headlessui/react";
 import { androidstudio } from '@uiw/codemirror-theme-androidstudio';
 import { javascript } from "@codemirror/lang-javascript";
 import { json } from "@codemirror/lang-json";
-import { createParser } from "js-parser";
+import { createParser } from "js-compiler";
 
 const codeMirrorJsExtensions = [javascript({ jsx: true })];
 const codeMirrorJSONExtensions = [json()];
@@ -11,6 +12,8 @@ const codeMirrorJSONExtensions = [json()];
 export function Playground() {
   const [code, setCode] = useState<string>("");
   const [astJson, setAstJson]= useState<string>("");
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>("");
   const onJsCodeMirrorChange = useCallback((code: string) => {
     setCode(code);
   }, []);
@@ -18,8 +21,11 @@ export function Playground() {
     try {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
         setAstJson(JSON.stringify(createParser(code).parse(), null, 4));
-    } catch(e) {
-        console.log(e);
+    } catch(e: any) {
+        setOpenModal(true);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        setErrorMsg(e.toString() as string);
+
     }
   }
   return (
@@ -80,6 +86,59 @@ export function Playground() {
                 </button>
             </div>
         </div>
+        <Transition appear show={isOpenModal} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => setOpenModal(false)}>
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                        Some Parser Error Happend
+                    </Dialog.Title>
+                    <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                            {JSON.stringify(errorMsg)}
+                        </p>
+                    </div>
+
+                    <div className="mt-4">
+                        <button
+                        type="button"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        onClick={() => setOpenModal(false)}
+                        >
+                        Close
+                        </button>
+                    </div>
+                    </Dialog.Panel>
+                </Transition.Child>
+                </div>
+            </div>
+            </Dialog>
+        </Transition>
     </div>
   )
 }
