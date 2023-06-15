177670ebabfed2294a226cfd8ec67b10b18e6cf4
Solo-steven <ssteven19@kimo.com>
2023-06-15 18:09:24 +0800

    feat: add un-commit part of code

--- packages/js-types/benchmark/index.ts
+++ packages/js-types/benchmark/index.ts
@@ -1,8 +1,18 @@
 import { traversal as DeclarativeTraversal } from "@/src/visitor/declarative";
 import { traversal as HalfInlineTraversal } from "@/src/visitor/half-inline";
 import { traversal as AllInlineTraversal } from "@/src/visitor/all-inline";
-import { validParent, benchmark, createFakeAST, BindingTable } from "./helper";
+import { validParent, createFakeAST, BindingTable } from "./helper";
 
+
+
+export function benchmark(callback: CallableFunction, prefix: string) {
+    const startStr = `${prefix}-start`;
+    const endStr = `${prefix}-end`;
+    performance.mark(startStr);
+    callback();
+    performance.mark(endStr);
+    console.log(performance.measure(prefix, startStr, endStr));
+}
 
 function frecurive(n: number): number {
     if(n === 0) {
@@ -13,8 +23,6 @@ function frecurive(n: number): number {
     }
     return frecurive(n-1) + frecurive(n-2);
 }
-
-
 
 
 function main() {
